9f9aab6773713de92f0d86d4144dbf7c1c66340f
liangyanxiang <liangyanxiang@juzix.io>
2019-03-14 18:35:58 +0800

    update

--- src/main.js
+++ src/main.js
@@ -30,42 +30,41 @@ Vue.use(VueI18n)
 //         next()
 //     }
 // });
-// window.addEventListener("load", async () => {
-//     // Modern dapp browsers...
-//     alert(window.imToken)
-//     if (window.ethereum) {
-//         alert(window.ethereum)
-//         window.web3 = new Web3(ethereum);
-//         try {
-//             // Request account access if needed
-//             await ethereum.enable();
-//             // Acccounts now exposed
-//             web3.eth.sendTransaction({
-//                 /* ... */
-//             });
-//         } catch (error) {
-//             // User denied account access...
-//         }
-//     }
-//     // Legacy dapp browsers...
-//     else if (window.web3) {
-//         alert(window.web3)
-//         window.web3 = new Web3(web3.currentProvider);
-//         // Acccounts always exposed
-//         web3.eth.sendTransaction({
-//             /* ... */
-//         });
-//     }
-//     // Non-dapp browsers...
-//     else {
-//         console.log(
-//             "Non-Ethereum browser detected. You should consider trying MetaMask!"
-//         );
-//         alert(
-//             "Non-Ethereum browser detected. You should consider trying MetaMask!"
-//         );
-//     }
-// });
+window.addEventListener("load", async () => {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+        alert(window.ethereum)
+        window.web3 = new Web3(ethereum);
+        try {
+            // Request account access if needed
+            await ethereum.enable();
+            // Acccounts now exposed
+            web3.eth.sendTransaction({
+                /* ... */
+            });
+        } catch (error) {
+            // User denied account access...
+        }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+        alert(window.web3)
+        window.web3 = new Web3(web3.currentProvider);
+        // Acccounts always exposed
+        web3.eth.sendTransaction({
+            /* ... */
+        });
+    }
+    // Non-dapp browsers...
+    else {
+        console.log(
+            "Non-Ethereum browser detected. You should consider trying MetaMask!"
+        );
+        alert(
+            "Non-Ethereum browser detected. You should consider trying MetaMask!"
+        );
+    }
+});
 // if (window.imToken) {
 //     alert(window.imToken)
 // } else {
